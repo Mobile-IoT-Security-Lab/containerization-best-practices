ba5db9af0261b0f7e62d1fb037d59da42ddcbb1d
Enrico Pezzano <53240167+enriicola@users.noreply.github.com>
2024-07-18 16:11:36 +0200

    Update good_cmd2.sh

--- cmd/good_cmd2.sh
+++ cmd/good_cmd2.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-echo "This is a good command"
+## -- Best practice 2: limit capabilities -- ##
 # docker run --cap-drop all --cap-add NET_BIND_SERVICE --cap-add SYS_CHROOT <image>
 docker run --cap-drop all --cap-add NET_BIND_SERVICE --cap-add SYS_CHROOT ubuntu:latest
