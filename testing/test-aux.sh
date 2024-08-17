ced7ff583a66d1b6f1638291db376eb667f570e1
EnricoPezzano <enrico.pezzano@outlook.it>
2024-08-17 10:22:16 +0200

    modified d-c 3

--- testing/test-aux.sh
+++ testing/test-aux.sh
@@ -2,12 +2,19 @@
 
 # RUN ME with "sh test-aux <docker-compose id>"
 
+clear
+
 if [[ $(uname -s) == "Darwin" ]]; then
    open -a Docker
    echo "Opening Docker Desktop..."
 fi
 
-sh test-dataset-example.sh $1 bad
-sh test-dataset-example.sh $1 good
+if [[ $DOCKER_CONTENT_TRUST == 0 ]]; then
+   export DOCKER_CONTENT_TRUST=1
+fi
+
+sh testing/test-dataset-example.sh $1 bad
+echo "--------------------------------------------------------------------------------------------"
+sh testing/test-dataset-example.sh $1 good
 
 # ids are from 0 to 35
--- testing/test-dataset-example.sh
+++ testing/test-dataset-example.sh
@@ -3,11 +3,11 @@
 # RUN ME with "sh test-dataset-example.sh <docker-compose id> <good|bad>"
 
 # initialize the path of the docker-compose files
-BAD_PATH="../docker-compose-files/bad-compose/bad_docker-compose"
-GOOD_PATH="../docker-compose-files/good-compose/good_docker-compose"
+BAD_PATH="docker-compose-files/bad-compose/bad_docker-compose"
+GOOD_PATH="docker-compose-files/good-compose/good_docker-compose"
 N=$1 #example number passed as argument
 EXT=".yaml"
-BENCHMARK_PATH="dockerprova/docker-compose.yml"
+BENCHMARK_PATH="testing/dockerprova/docker-compose.yml"
 
 
 # Check if the argument is "good" or "bad"
@@ -15,6 +15,7 @@ if [ $2 = "good" ]; then
     MY_PATH=$GOOD_PATH$N$EXT
 else
     MY_PATH=$BAD_PATH$N$EXT
+    # code $MY_PATH
 fi
 
 # Copy the selected docker-compose file to the benchmark path
@@ -25,7 +26,7 @@ docker images | awk 'NR>1{print $3}' | xargs docker rmi -f
 docker container ls -a | awk 'NR>1{print $1}' | xargs docker rm -f
 
 # Run and build the docker-compose file
-cd dockerprova
+cd testing/dockerprova
 docker-compose up --build
 
 # Run the dockle tests
@@ -36,4 +37,4 @@ docker images | awk 'NR>1{print $1}' | xargs dockle
 # Run the trivy tests
 echo; echo
 echo "\033[1m TRIVY RESULTS for the $2 image: \033[0m"
-docker images | awk 'NR>1{print $3}' | xargs trivy image
+docker images | awk 'NR>1{print $1}' | xargs trivy image
